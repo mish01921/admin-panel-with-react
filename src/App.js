7590bac3ae77a51307687c831e19a87036a50d2a
mish01921 <misho191524@gmail.com>
2022-08-27 23:47:14 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 <div>
   <AdminM />
   </div>
-
+  
   );
 }
 
--- src/ComponentM/adminM.js
+++ src/ComponentM/adminM.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React from 'react';
 import "./Misho.css"
- import CreateM from './createM';
- import ReadM from './readM';
-//  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import CreateM from './createM';
+import ReadM from './readM';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import UpdateM from './updateM';
 
-
-
-function AdminM() {
+export default function AdminM() {
   return (
-   
-    
-     <div className="main">
-      <h2 className="main-header">React Crud Operations</h2>
-      <div>
-         <CreateM />
+    <Router>
+      <div className="main">
+        <h2 className="main-header">React Crud Operations</h2>
+        <div>
+          <Routes>
+             <Route path='/createM' element={<CreateM />} />
+          </Routes>
+        </div>
+
+        <div style={{ marginTop: 20 }}>
+           <Routes>
+             <Route path='/readM' element={<ReadM />} />
+          </Routes>
+        </div>
+
+        <div>
+          <Routes>
+             <Route path='/updateM' element={<UpdateM />} />
+          </Routes>
+        </div>
+        
       </div>
-
-      <div style={{ marginTop: 20 }}>
-      <ReadM />
-      </div>
-    </div>
-
-
- 
-  )
+    </Router>
+  );
 }
-
-export default AdminM;
