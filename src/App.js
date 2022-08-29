42eb33427ee9a8062ff7b6c6b605fff286ef920c
VachMalkhasyan <vachmalkhasyan@gmail.com>
2022-08-29 11:28:36 +0400

    to survive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 // <<<<<<< HEAD
-import Lilit from './Lilit_Component/Lilit';
-=======
->>>>>>> 0dd18b72cf5f3758925ce3bbd58e15c9c8f15a16
+
+
 
 import Kristina from "./KristinaComponents/Kristina";
 
@@ -12,51 +10,37 @@ import Vach from './VachComponents/Vach'
 
 import React, {useState}  from 'react';
 
-import './App.css';
+// import './App.css';
 // import Elen from './ElenComponents./Elen';
 import Elen from './ElenComponents/Elen';
 //misho components
-import AdminM from './ComponentM/adminM';
+ import AdminM from './ComponentM/adminM';
+
+
+
+
 
 
 
 function App() {
- 
   return (
-=======
-import React from "react";
-import AdminM from "./ComponentM/adminM";
 
-<<<<<<< HEAD
-function App() {
-  return (
-<div>
-  <AdminM />
-  </div>
   
-  );
-}
->>>>>>> misho
+ 
 
-    // <div>
+
+
+   
   
     <div className='App'>
        <Elen />
-=======
-    <div className='App'>
       <Kristina />
->>>>>>> 0dd18b72cf5f3758925ce3bbd58e15c9c8f15a16
       <Lilit />
       <Vach />
-      <Elen />
       <AdminM />
     </div>
-<<<<<<< HEAD
-  )
-}
+
+)}
     
-=======
-  );
-} 
->>>>>>> 0dd18b72cf5f3758925ce3bbd58e15c9c8f15a16
+  
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
- import 'semantic-ui-css/semantic.min.css';
-=======
-import 'semantic-ui-css/semantic.min.css';
->>>>>>> 0dd18b72cf5f3758925ce3bbd58e15c9c8f15a16
+//  import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
+
 
 
 
@@ -15,23 +13,22 @@ root.render(
     <App />
 
   </React.StrictMode>
-<<<<<<< HEAD
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
 
 
-);
+
+
 
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
->>>>>>> 0dd18b72cf5f3758925ce3bbd58e15c9c8f15a16
+
 
 
 // If you want to start measuring performance in your app, pass a function
@@ -40,12 +37,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
-<<<<<<< HEAD
-    
-=======
 
-
-
-
-
->>>>>>> 0dd18b72cf5f3758925ce3bbd58e15c9c8f15a16
