41480b2047f387924fa274948c2d2d70cd1600bf
mish01921 <misho191524@gmail.com>
2022-08-31 17:02:37 +0400

    add new ComponentM

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Lilit from './Lilit_Component/Lilit';
 import Kristina from "./KristinaComponents/Kristina";
 import Vach from './VachComponents/Vach';
 import Elen from './ElenComponents/Elen';
+import AdminM from "./ComponentM/AdminM"
 import './App.css';
 
 
@@ -21,6 +22,7 @@ function App() {
         <Route path='/Elen' element={<Elen />} />
         <Route path='/Lilit' element={<Lilit />} />
         <Route path='/Kristina' element={<Kristina />} />
+        <Route path='/AdminM' element={<AdminM />} />
       </Routes>
     </BrowserRouter>
     
---
+++ src/ComponentM/AdminM.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdminM() {
+  return (
+    <div>adminM</div>
+  )
+}
+
+export default AdminM
